6ebb32032d79b8d5540cab2e0710a2614bfb45ed
DAT TRUONG <quoc3813@gmail.com>
2023-03-22 04:25:17 +0700

    hoan thanh tuan 2

--- 2011062879_TruongLeQuocDat/Controllers/CoursesController.cs
+++ 2011062879_TruongLeQuocDat/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using _2011062879_TruongLeQuocDat.Models;
 using _2011062879_TruongLeQuocDat.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,5 +26,27 @@ namespace _2011062879_TruongLeQuocDat.Controllers
             };
             return View(viewModel);
         }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create",viewModel);
+            }
+            var course = new Course
+        {
+            LecturerId = User.Identity.GetUserId(),
+            DateTime = viewModel.GetDateTime(),
+            CategoryId = viewModel.Category,
+            Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+        
     }
 }
--- 2011062879_TruongLeQuocDat/Controllers/HomeController.cs
+++ 2011062879_TruongLeQuocDat/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using _2011062879_TruongLeQuocDat.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
 
 namespace _2011062879_TruongLeQuocDat.Controllers
 {
+    
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbcontext;
+        public HomeController()
+        {
+            _dbcontext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcommingCourses = _dbcontext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourses);
         }
 
         public ActionResult About()
